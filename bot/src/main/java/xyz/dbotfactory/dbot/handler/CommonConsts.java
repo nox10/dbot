62afa0ba02d276df19a8fe918188445f93f5aada
N <n>
2020-10-28 00:03:43 +0300

    Language

--- bot/src/main/java/xyz/dbotfactory/dbot/handler/CommonConsts.java
+++ bot/src/main/java/xyz/dbotfactory/dbot/handler/CommonConsts.java
@@ -12,13 +12,13 @@ public interface CommonConsts {
     String CUSTOM_SHARE_CALLBACK_DATA = "customshare";
     String SETING_CUSTOM_SHARE_METADATA = "settingcustshr";
     String DONE_MESSAGE_TEXT = "💸 Now please send me how much you have paid already, right in this chat.\n\n" +
-            "ℹ️ Total sum is: ";
-    String GO_TO_GROUP_TEXT = "🔙 Please, return to group chat";
+            "ℹ️ Just to remind you, the total sum is: ";
+    String GO_TO_GROUP_TEXT = "🔙 Please return to the group chat";
     String ITEMS_MESSAGE_TEXT = "🧾 Tap to items which are yours";
     String YOUR_RECEIPT_TEXT = "<b>🧾 Your receipt:\n</b>";
     String DONE_TEXT = "ℹ️ Feel free to send more items or press button below to continue";
     String SQUARED_DONE_EMOJI = "ℹ️";
-    String CHECK_STATUS_BUTTON_TEXT = SQUARED_DONE_EMOJI + " Check status " + SQUARED_DONE_EMOJI;
+    String CHECK_STATUS_BUTTON_TEXT = SQUARED_DONE_EMOJI + " Check the status " + SQUARED_DONE_EMOJI;
     String SHARES_DONE_TASK_NAME = "shares_done_task";
     String RECEIPT_BALANCES_BUILT = "rceipt_balances_built";
     String RECEIPT_LINE = "=====================";
